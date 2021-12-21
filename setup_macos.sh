25daad9fc51cdda674e6b274ad1327d9306ac78b
David Neumann <davidjneumann@gmail.com>
2021-12-21 07:38:39 -0500

    Setup iterm2 prompt in macos

--- setup_macos.sh
+++ setup_macos.sh
@@ -1,3 +1,47 @@
+#Add helpful brew stuff
+if ! command -v brew &> /dev/null
+then
+    echo "Installing brew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+brew tap homebrew/cask
+if ! command -v git &> /dev/null
+then
+    echo "Installing git"
+    brew install git
+fi
+if ! mdfind "kMDItemCFBundleIdentifier == com.googlecode.iterm2" &> /dev/null
+then
+    echo "Installing iterm2"
+brew install --cask iterm2
+fi
+
+#Setup oh-my-posh
+if ! fc-list | grep "Meslo" &> /dev/null
+then
+    here=$(pwd)
+    echo "Installing Meslo font"
+    # clone
+    git clone https://github.com/powerline/fonts.git --depth=1 /tmp/fonts/
+    # install
+    cd /tmp/fonts/
+    ./install.sh
+    # clean-up a bit
+    cd ..
+    rm -rf fonts
+    cd $here
+fi
+if ! command -v oh-my-posh  &> /dev/null
+then
+    echo "Installing oh-my-posh"
+    brew tap jandedobbeleer/oh-my-posh
+    brew install oh-my-posh
+    cp /usr/local/opt/oh-my-posh/themes/emodipt.omp.json ~/
+    posh_string='eval "$(oh-my-posh --init --shell bash --config ~/emodipt.omp.json)"'
+    grep -qxF "$posh_string" ~/.bashrc || echo "$posh_string" >> ~/.bashrc
+    . ~/.bashrc
+fi
+
 #Setup zsh or bash completions
 if [[ $OSTYPE == 'darwin'* ]]; then
     echo "Enable subpixel antialiasing for bad montiors"
