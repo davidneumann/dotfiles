7b776bd76cdc62f66ed9ed52d592c6561ee4ef25
David Neumann <davidjneumann@gmail.com>
2023-06-26 19:11:13 -0400

    Create ~/.config/wezterm if required

--- wezterm/install.sh
+++ wezterm/install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # Make symbolic links for lua items here to ~/.config/wezterm/
-for f in `find . -regex ".*\.lua$"`; do
-    rm -rf ~/.config/wezterm/$f
-    ln -s $(pwd)/$f ~/.config/wezterm/$f
+#for f in `find . -regex ".*\.lua$"`; do
+# Now without leading ./
+mkdir -p $HOME/.config/wezterm/
+for f in `find . -type f -regex ".*\.lua$" -printf '%P\n'`; do
+    rm -rf $HOME/.config/wezterm/$f
+    ln -s $(pwd)/$f $HOME/.config/wezterm/$f
 done
