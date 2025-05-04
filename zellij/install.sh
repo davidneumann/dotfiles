a5bbdf2a0f16db53e03e4f8624fd42158c242c89
David Neumann <davidjneumann@gmail.com>
2025-05-04 12:51:54 -0400

    Add zellij config and install script

---
+++ zellij/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+
+mkdir -p ~/.config/zellij/plugins
+wget https://github.com/laperlej/zellij-choose-tree/releases/latest/download/zellij-choose-tree.wasm -O ~/.config/zellij/plugins/zellij-choose-tree.wasm
+
+wget https://github.com/laperlej/zellij-sessionizer/releases/latest/download/zellij-sessionizer.wasm -O ~/.config/zellij/plugins/zellij-sessionizer.wasm
