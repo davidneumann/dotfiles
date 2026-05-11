81feb4f3d3778bab11747858811f36ece9300a78
David Neumann <davidjneumann@gmail.com>
2026-05-11 19:54:39 -0400

    Add mac alt left/right support to wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,17 +8,17 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-	config = wezterm.config_builder()
+  config = wezterm.config_builder()
 end
 
 -- Nixos fix. TODO: Remove when safe
 config.front_end = "WebGpu"
 
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 
 config.hide_tab_bar_if_only_one_tab = true
@@ -28,27 +28,32 @@ config.hide_tab_bar_if_only_one_tab = true
 -- -- For example, changing the color scheme:
 -- config.color_scheme = 'AdventureTime'
 
--- config.keys = {
---   -- paste from the clipboard
---   { key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
--- }
+config.keys = {
+  -- -- paste from the clipboard
+  -- { key = 'V', mods = 'CTRL', action = act.PasteFrom 'Clipboard' },
+
+  -- Option+LeftArrow to move one word left
+  { key = 'LeftArrow',  mods = 'ALT', action = wezterm.action { SendString = '\x1bb' } },
+  -- Option+RightArrow to move one word right
+  { key = 'RightArrow', mods = 'ALT', action = wezterm.action { SendString = '\x1bf' } },
+}
 
 config.color_scheme = "tokyonight_night"
 
 local launch_menu = {}
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.default_domain = "WSL:Ubuntu"
-	config.keys = {
-		{ key = "m", mods = "ALT", action = wezterm.action.ShowLauncher },
-	}
-	config.font = wezterm.font("MesloLGM NF")
+  config.default_domain = "WSL:Ubuntu"
+  config.keys = {
+    { key = "m", mods = "ALT", action = wezterm.action.ShowLauncher },
+  }
+  config.font = wezterm.font("MesloLGM NF")
 
-	table.insert(launch_menu, {
-		label = "PowerShell 7",
-		args = { "pwsh.exe", "-NoLogo" },
-		-- "C:\Program Files\PowerShell\7\pwsh.exe" -WorkingDirectory ~
-		domain = { DomainName = "local" },
-	})
+  table.insert(launch_menu, {
+    label = "PowerShell 7",
+    args = { "pwsh.exe", "-NoLogo" },
+    -- "C:\Program Files\PowerShell\7\pwsh.exe" -WorkingDirectory ~
+    domain = { DomainName = "local" },
+  })
 end
 config.launch_menu = launch_menu
 
